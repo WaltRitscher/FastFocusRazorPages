6477f8f5ca6e8cd0ab3a214e06ee9dc7df8113e6
Walt Ritscher <waltritscher@outlook.com>
2018-04-30 23:56:29 -0700

    Add CSS and Layout Add Databinding Pages

---
+++ FastFocusApp/Pages/DatabindToSingleItem.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TailorMadeTours.Models;
+
+namespace FastFocusApp.Pages
+{
+  public class DatabindToSingleItemModel : PageModel
+  {
+    public void OnGet()
+    {
+      CurrentTourStop = TailorMadeTours.Models.TourSource.TourStops.ElementAt(3);
+
+    }
+
+    public TourStop CurrentTourStop { get; set; }
+  }
+}
---
+++ FastFocusApp/Pages/DatabindWithRouteParam.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TailorMadeTours.Models;
+
+namespace FastFocusApp.Pages
+{
+    public class DatabindWithRouteParamModel : PageModel
+    {
+    public void OnGet(int id)
+    {
+      CurrentTourStop = TailorMadeTours.Models.TourSource.TourStops.ElementAt(id);
+
+    }
+    [BindProperty]
+    public TourStop CurrentTourStop { get; set; }
+  }
+}
